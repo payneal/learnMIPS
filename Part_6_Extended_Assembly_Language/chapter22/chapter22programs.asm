61d6b27e99950eae80bacb128b85f8958704b0ba
Ali Payne <payneal@onid.oregonstate.edu>
2016-01-21 09:09:28 -0500

    on ch22 programs

---
+++ Part_6_Extended_Assembly_Language/chapter22/chapter22programs.asm
@@ -0,0 +1,22 @@
+        .text 
+        .globl main
+
+main: 
+
+    ## ex.1 
+
+    ## Write a program that repeatedly prompts the user for the number of miles traveled and the gallons of gasoline consumed, and then prints out the miles per gallon. Use integer math. Exit when the user enters 0 for the number of miles.
+
+
+    ## ex.2
+    
+    ## As in Exercise1, write a program that repeatedly prompts the user for the number of miles traveled and the gallons of gasoline consumed, and then prints out the miles per gallon. Exit when the user enters 0 for the number of miles.
+
+    ## Use fixed-point arithmetic for this. Multiply input value of miles by 100. Divide miles by gallons to get miles per gallon. The answer will 100 times too large. So divide it by 100 to get whole miles per gallon in hi and hundredths in lo. Print out lo followed by "." followed by hi to get an output that looks like: "32.45 mpg".
+
+    ## ex.3 
+
+    ## 
+
+
+## end of program
