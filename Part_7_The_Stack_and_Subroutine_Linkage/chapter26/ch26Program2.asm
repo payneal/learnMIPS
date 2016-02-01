3039592d91e5386f648fa83025431a90dbe4969f
Ali Payne <payneal@onid.oregonstate.edu>
2016-02-01 00:08:59 -0500

    didnt follow progam 2 directions had to edit, need to verify if it works

--- Part_7_The_Stack_and_Subroutine_Linkage/chapter26/ch26Program2.asm
+++ Part_7_The_Stack_and_Subroutine_Linkage/chapter26/ch26Program2.asm
@@ -11,16 +11,26 @@ main:
 
 #Use the subroutine in a program that computes the sum of a list of integers that the user enters. The user signals the end of input by typing "done".
 
+    li        $s3,2
     
+theloop:
     #ask user for an int
     jal         askForInt
     nop
     
     #see if valid or not
-    beq        $v1, $0, bad
-    
-    move       $s0, $v0         # save total in $s0
- 
+    beq        $v1, $s3, done 
+    bne        $v1, $0, good
+   
+    li          $v0, 4 
+    la          $a0, error
+    syscall 
+
+good:
+    addu       $s0, $s0,  $v0   #  add to total in $s0
+    j          theloop 
+
+done: 
     li          $v0, 4          # "your total" 
     la          $a0, total
     syscall
@@ -34,17 +44,10 @@ main:
     li          $v0, 10
     syscall
     
-  bad: 
-    li          $v0, 4 
-    la          $a0, error
-    syscall 
-
-    j           exit
-    
 ## end of main
     .data
-error:  .asciiz     "error: you entered a non int"
-total:  .asciiz     "int(string)=  "
+error:  .asciiz     "error: you entered a non int\n"
+total:  .asciiz     "total= "
 
 ###########################
 ###### askForInt ##########
@@ -61,16 +64,18 @@ askForInt:
     la          $a0, space
     li          $v0, 8 
     syscall
-
+        
     la           $t0, 10      # linux end of string
     la           $t1, 0         # total == 0
 
-    # go through entered string make sure its string    
+    # go through entered string make sure its a number    
   loop: 
     lb           $t2, 0($a0)    # put letter in $t1
     beq          $t2, $t0, gotChars    
-    
+        
     bne          $t1, $0, notFirstChar
+    li           $t9, 0x64      # d
+    beq          $t2, $t9, over # if char[0] =='d'
     li           $t9, 0x2d
     bne          $t2, $t9, notFirstChar
     li           $t8, 1         # lets me know its neg 
@@ -97,9 +102,41 @@ notFirstChar:
     move        $v0, $t1
     jr          $ra
 
-  badEntry: 
+  badEntry:
+    
+    li          $v0, 4
+    la          $a0, errormsg
+    syscall
+
     li          $v0, 0          # input no convert v1 = 0
     li          $v1, 0
+    jr          $ra
+
+  over:
+    #make sure full word is done
+    addiu       $a0, $a0, 1     # next char 
+    lb          $t2, 0($a0)
+    li          $t7, 0x6F       # 'o'
+    bne         $t2, $t7, badEntry
+
+    addiu       $a0, $a0, 1     # next char 
+    lb          $t2, 0($a0)
+    li          $t7, 0x6E       # 'n'
+    bne         $t2, $t7, badEntry
+
+    addiu       $a0, $a0, 1     # next char 
+    lb          $t2, 0($a0)
+    li          $t7, 0x65       # 'e'
+    bne         $t2, $t7, badEntry
+
+    addiu       $a0, $a0, 1     # next char 
+    lb          $t2, 0($a0)
+    li          $t7, 10         # unix wnd of sting
+    bne         $t2, $t7, badEntry
+
+    # makes it here then user entered done
+    li          $v0, 0 
+    li          $v1, 2
     jr          $ra
 
         .data
@@ -107,7 +144,8 @@ askInt: .asciiz         "enter an int: "
 space:  .space          200
 entered:.asciiz         "you entered: " 
 sayOr:     .asciiz         " or " 
-
+itsover:.asciiz         "\n"
+errormsg: .asciiz       "bad entry\n"
 ## end of askForInt    
 
 
