c2ec57af7411097d9ae2b1693ba67c5d823d1c26
Ali Payne <ali.payne@zipscene.com>
2016-02-12 10:53:12 -0500

    finished last program

--- part_9_data_structures/ch36/program5.asm
+++ part_9_data_structures/ch36/program5.asm
@@ -23,33 +23,133 @@ main:
 
           # ask for the number
           li      $v0, 4              # print string
-          la      $a0, askNumb        #
+          la      $a0, askNum         #
           syscall
 
           li      $v0, 5              # read int
           syscall
-
           move    $a0, $v0            # put number entered as arguement 
 
           # object1.set(); 
           lw      $a1, object1        # get address of object1
-          lw      $t0,0($a0)          # get address of set method 
+          lw      $t0,0($a1)          # get address of set method 
           jalr    $t0                 # call the method 
-            
+          nop            
+
           # object.print(); 
           lw      $a0, object1        # get address of object1
           lw      $t0,8($a0)          # get address of print method 
           jalr    $t0
- 
+          nop 
+
           # object1.double(); 
-          #lw      $a0, object1        # get address of object1
-          #lw      $t0,4($a0)          # get address of double method 
-          #jalr    $t0  
+          lw      $a0, object1        # get address of object1
+          lw      $t0,4($a0)          # get address of double method 
+          jalr    $t0  
+          nop          
 
           # object.print(); 
-          #lw      $a0, object1        # get address of object1
-          #lw      $t0,8($a0)          # get address of print method 
-          #jalr    $t0
+          lw      $a0, object1        # get address of object1
+          lw      $t0,8($a0)          # get address of print method 
+          jalr    $t0
+          nop 
+
+        # object 2
+        ###########################
+                                      # OBJECT 2 = NEW OBJECT(); 
+          li      $v0, 9              # allocate 16 bytes 
+          li      $a0, 16             #
+          syscall                     # $v0 = address 
+          sw      $v0, object2        #
+
+          la      $t0, set            # initialize jump table
+          sw      $t0,0($v0)          #
+          la      $t0, double         #
+          sw      $t0,4($v0)          #
+          la      $t0, print          #
+          sw      $t0,8($v0)          #
+
+          # ask for the number
+          li      $v0, 4              # print string
+          la      $a0, askNum         #
+          syscall
+
+          li      $v0, 5              # read int
+          syscall
+          move    $a0, $v0            # put number entered as arguement 
+
+          # object2.set(); 
+          lw      $a1, object2        # get address of object1
+          lw      $t0,0($a1)          # get address of set method 
+          jalr    $t0                 # call the method 
+          nop            
+
+          # object2.print(); 
+          lw      $a0, object2        # get address of object1
+          lw      $t0,8($a0)          # get address of print method 
+          jalr    $t0
+          nop 
+
+          # object2.double(); 
+          lw      $a0, object2        # get address of object1
+          lw      $t0,4($a0)          # get address of double method 
+          jalr    $t0  
+          nop          
+
+          # object2.print(); 
+          lw      $a0, object2        # get address of object1
+          lw      $t0,8($a0)          # get address of print method 
+          jalr    $t0
+          nop
+
+        # object 3
+        ############################
+        
+                                      # OBJECT 3 = NEW OBJECT(); 
+          li      $v0, 9              # allocate 16 bytes 
+          li      $a0, 16             #
+          syscall                     # $v0 = address 
+          sw      $v0, object3        #
+
+          la      $t0, set            # initialize jump table
+          sw      $t0,0($v0)          #
+          la      $t0, double         #
+          sw      $t0,4($v0)          #
+          la      $t0, print          #
+          sw      $t0,8($v0)          #
+
+          # ask for the number
+          li      $v0, 4              # print string
+          la      $a0, askNum         #
+          syscall
+
+          li      $v0, 5              # read int
+          syscall
+          move    $a0, $v0            # put number entered as arguement 
+
+          # object3.set(); 
+          lw      $a1, object3        # get address of object1
+          lw      $t0,0($a1)          # get address of set method 
+          jalr    $t0                 # call the method 
+          nop            
+
+          # object3.print(); 
+          lw      $a0, object3        # get address of object1
+          lw      $t0,8($a0)          # get address of print method 
+          jalr    $t0
+          nop 
+
+          # object3.double(); 
+          lw      $a0, object3        # get address of object1
+          lw      $t0,4($a0)          # get address of double method 
+          jalr    $t0  
+          nop          
+
+          # object3.print(); 
+          lw      $a0, object3        # get address of object1
+          lw      $t0,8($a0)          # get address of print method 
+          jalr    $t0
+          nop
 
           # exit program
           li      $v0,10              # return to OS
@@ -72,10 +172,10 @@ askNum:    .asciiz      "what is the number for this object: "
 set: 
         move     $s1,$a1           # save objects address 
          
-        addiu    $a1, $s1, 12      # $a1 = address of where objects value is stored 
+        addiu    $a1, $a1, 12      # $a1 = address of where objects value is stored 
  
         sw       $a0,0($a1)        # save the value
-                 
+          
         jr       $ra               # return to caller
 
         .data
@@ -85,7 +185,12 @@ ask:    .asciiz     ""
 # double() method
         .text
 double:
-           
+        addiu   $a0, $a0, 12    # address of objetcs int
+        lw      $t0, 0($a0)     # load the number
+       
+        addu   $t0, $t0, $t0   # double value
+        
+        sw      $t0, 0($a0) 
         
         jr       $ra             # return to caller
 
@@ -97,6 +202,7 @@ double:
 print:
         li        $v0, 1         # print int
         addiu     $a0, $a0, 12   # address of objects int 
+        lw        $a0, 0($a0)    # load the number 
         syscall     
 
         li        $v0, 4        # print  string
@@ -104,7 +210,7 @@ print:
         syscall
         
         jr       $ra             # return to caller
-
+        nop 
         .data
 newL:   .asciiz     "\n"
 #######################################
