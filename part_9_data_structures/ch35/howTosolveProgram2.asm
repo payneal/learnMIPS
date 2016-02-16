e19964d9a0d04b678d6086fbea574d58d5992eb1
Ali Payne <payneal@onid.oregonstate.edu>
2016-02-15 20:17:31 -0500

    IDK in theory it should world but the newly allocated data that is created for the sting is data that has already been assigned

--- part_9_data_structures/ch35/howTosolveProgram2.asm
+++ part_9_data_structures/ch35/howTosolveProgram2.asm
@@ -3,58 +3,98 @@
 
 
 main: 
+    
+    # create the first linked list
+    
+    li  $v0, 9          # need to request memory
+    li  $a0, 8          # 8 bytes of memory
+    syscall 
+    move $s0,$v0        # now $s0 holds first link
+     
+    move   $s1, $s0        # now $s1 holds start of first link 
 
- li $v0, 4
- la $a0, enter
- syscall 
+    # first word stores word
+    # 2nd word stores location of next node
+   
+    # get first string
+    li $v0, 4           # code 4 = print
+    la $a0, enter       # "enter a string
+    syscall             
 
- li $v0, 8 
- syscall
+    li $a1, 17          # only hold 16 chars 
+    li $v0, 8           # collect the entered string
+    syscall
 
-move $t0, $a0  
+    move $t0, $a0       # hold string at $t0 
+    
+    li  $v0, 9           # need to request memory
+    li  $a0, 17          # hold 16 bytes  for 16 char string
+    syscall 
+ 
+    sw $t0,0($v0)       # store the string in the created memory
+ 
+    sw  $v0,0($s0)      # store address of string @ first 4 bytes of linked list
 
-li  $v0, 9 
-li  $a0, 200
-syscall
+    # create second linked list
+    li  $v0, 9          # need to request memory
+    li  $a0, 8          # 200 bytes to be exact idk why I did that
+    syscall
 
-sw $t0,0($v0)
+    sw  $v0,4($s0)      # store the next linked list adress in Linked list1
 
-sw $v0, test
+   move $s0,$v0         # makes $s0 now hold newly created linked list
+    
+    
+    # ask for new string
+    li $v0, 4           # code 4 = print
+    la $a0, enter       # "enter a string
+    syscall             
 
-move $t8, $v0
+    li $v0, 8           # collect the entered string
+    syscall
 
- li $v0, 4
- la $a0, you
- syscall 
+    move $t0, $a0       # hold string at $t0 
 
-lw $a0, test
-lw $a0, ($a0)
-li $v0, 4
-syscall
+    li  $v0, 9          # need to request memory
+    li  $a0, 17         # 17 bytes to hold 16 chars
+    syscall
 
-lw $a0, test
-li $v0, 4
-syscall
+    sw $t0,0($v0)       # store the string in the created memory
+    
+    sw $v0,0($s0)       # store address of string @ first 4 of linked list
 
-lw  $a0, newL
-li  $v0, 4 # print char
-syscall
+    # im ending it here so
+    sw $0,4($s0)        # this linked list points to null
+    
 
+    # now try to print everything
+    move     $t0, $s1
+    lw     $t1, 0($t0)  # the adress of the 1st linked list string
+    lw     $t2, 4($t0)  # the adress of the next linked list
 
-lw  $a0,0($t0) 
-li  $v0, 4 # print char
-syscall
+  loop: 
+    beq    $t2, $0, done    # if the next linked list adress == null then done
+    
+    li      $v0, 4      # code 4 == print string 
+    la      $a0, you    # "you entered the following"
+    syscall
+    
+    lw      $a0, 0 ($t0) 
+    syscall 
 
+    la      $a0, newL
+    syscall 
 
-lw  $a0, newL
-li  $v0, 4 # print char
-syscall
-
-
-lw  $a0,($t8) 
-li  $v0, 1 # print char
-syscall
-
+    move    $t0, $t2
+    lw     $t1, 0($t0)  # the adress of the 1st linked list string
+    lw     $t2, 4($t0)  # the adress of the next linked list
+    j       loop
+    nop
+       
+done: 
+    # exit program
+    li      $v0,10            # return to OS
+    syscall       
 
         .data
 enter:  .asciiz     "enter a string: "
